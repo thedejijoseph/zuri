fdb9766cf9f9336fed77b0be69b5a602da7aed36
Deji <wrecodde@gmail.com>
2021-04-08 14:35:08 +0100

    making corrections to task 2

--- task-02/README.md
+++ task-02/README.md
@@ -3,3 +3,8 @@ Create a php page that is able to perform basic calculation: addition, subtracti
 Answers.. here, really (index.php). Still tracking progress via Notion, so.. the link: https://www.notion.so/wrecodde/Task-4-faf9e607d54e44c296ac6c28ba989886
 
 Also made the parent page, all Tasks, public too..
+
+
+08042021; 1424
+So, I failed this exercise spectacularly: 1/4. I'll make some corrections, make an implementation of how I understood the intended solution from corrections they made...
+
--- task-02/index.php
+++ task-02/index.php
@@ -2,34 +2,38 @@
 
 function opAddition($var1, $var2){
     $result = $var1 + $var2;
-    echo $result, "\n";
+    return $result;
 }
 
 function opSubtraction($var1, $var2){
     $result = $var1 - $var2;
-    echo $result, "\n";
+    return $result;
 }
 
 function opMultiplication($var1, $var2){
     $result = $var1 * $var2;
-    echo $result, "\n";
+    return $result;
 }
 
 function opDivision($var1, $var2){
     $result = $var1 / $var2;
-    echo $result, "\n";
+    return $result;
 }
 
 function opModulus($var1, $var2){
     $result = $var1 % $var2;
-    echo $result, "\n";
+    return $result;
 }
 
+// since I still don't know how to get input from the command line..
+$var1 = 9;
+$var2 = 5;
 
-opAddition(3, 4);
-opSubtraction(5, 3);
-opMultiplication(2, 6);
-opDivision(8, 4);
-opModulus(9, 4);
+echo("Implementing the following operations on two variables: $var1 & $var2 \n");
+echo("Addition (+): ".opAddition($var1, $var2)."\n");
+echo("Subtraction (-): ".opSubtraction($var1, $var2)."\n");
+echo("Multiplication (*): ".opMultiplication($var1, $var2)."\n");
+echo("Division (/): ".opDivision($var1, $var2)."\n");
+echo("Modulus (%): ".opModulus($var1, $var2)."\n");
 
 ?>
